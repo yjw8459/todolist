4fee17bbbdb1617ec0f3bd4959e8ea4021df70d5
yjw8459 <ryu01410@gmail.com>
2021-11-07 23:48:34 +0900

    Initial commit

---
+++ src/main/kotlin/com/yjw/todolist/TodolistApplication.kt
@@ -0,0 +1,11 @@
+package com.yjw.todolist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodolistApplication
+//dsaas
+fun main(args: Array<String>) {
+	runApplication<TodolistApplication>(*args)
+}
